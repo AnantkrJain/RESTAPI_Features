b9303887b600bdfdf82afc58eebb834f7cbdbe04
AnantkrJain <anantkr.developer@gmail.com>
2020-05-03 11:55:32 +0530

    spring boot framework's basic structure to work on

---
+++ springboot-basic-steps/src/main/java/com/wipro/springboot/basics/springbootbasicsteps/SpringbootBasicStepsApplication.java
@@ -0,0 +1,18 @@
+package com.wipro.springboot.basics.springbootbasicsteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringbootBasicStepsApplication {
+
+	public static void main(String[] args) {
+		//ApplicationContext applicationContext = 
+				SpringApplication.run(SpringbootBasicStepsApplication.class, args);
+		//for(String name: applicationContext.getBeanDefinitionNames()) {
+			//System.out.println(name);
+		//}
+	}
+	
+}
---
+++ springboot-basic-steps/src/test/java/com/wipro/springboot/basics/springbootbasicsteps/SpringbootBasicStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.springboot.basics.springbootbasicsteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBasicStepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
